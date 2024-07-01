79bba39635ab62b99894a78e87307cbd9a521e57
himan1009 <shishusha922@gmail.com>
2024-07-01 12:31:32 +0530

    first commit

---
+++ class1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Teacher{
+    // properties / attributes
+    string name;
+    string dept;
+    string subject;
+    double salary;
+
+    // methods or member functions
+    void changeDept(string newDept){
+        dept=newDept;
+    }
+};
+
+int main(){
+    Teacher T1;
+    ~
+    return 0;
+}
